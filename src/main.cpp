61a2ef54d5267e7c2596f06d4ed4428563448b78
MarieMag <mariemag.bonnet@gmail.com>
2024-01-31 09:18:02 +0100

    constantes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include "WiFiClientSecure.h"
+#include <PubSubClient.h>
+
+const char* wifi_ssid = "la ratiera";
+const char* wifi_password = "swing000";
+const char* mqtt_server = "27cc61dbaffc4da08cd0081cabd8cf01.s2.eu.hivemq.cloud";
+int mqtt_port = 8883;
+const char* mqtt_user = "ocres4ever";
+const char* mqtt_pass = "ocresse123";
+const char* client_id = "TD01_GP03";
 
 // Define the pins that we will use
 #define SENSOR 33
